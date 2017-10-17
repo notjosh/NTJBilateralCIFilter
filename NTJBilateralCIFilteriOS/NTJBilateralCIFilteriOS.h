bb0456f57f243816101af1d6f3ab5209c4b41ed4
Joshua May <notjosh@gmail.com>
2017-10-17 23:04:57 +0200

    Xcode 9

---
+++ NTJBilateralCIFilteriOS/NTJBilateralCIFilteriOS.h
@@ -0,0 +1,14 @@
+//
+//  NTJBilateralCIFilteriOS.h
+//  NTJBilateralCIFilter
+//
+//  Created by Joshua May on 17/10/17.
+//  Copyright © 2017 nojo inc. All rights reserved.
+//
+
+#ifndef NTJBilateralCIFilteriOS_h
+#define NTJBilateralCIFilteriOS_h
+
+#import "NTJBilateralCIFilter.h"
+
+#endif /* NTJBilateralCIFilteriOS_h */
